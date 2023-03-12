e0ea2782ac737d069b522f9745eae0bcef0183d0
Florencia D'Andrea <florencia.dandrea@gmail.com>
2023-03-12 15:12:58 -0700

    Add new tests

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,39 +1,35 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: haiwoo_province", {
-  app <- AppDriver$new(name = "haiwoo_province", height = 789, width = 1169)
+test_that("{shinytest2} recording: comrav_by-province", {
+  app <- AppDriver$new(variant = platform_variant(), name = "comrav_by-province", 
+      height = 743, width = 788)
   app$set_inputs(navbar = "by Province")
-  app$set_inputs(species2 = "haiwoo")
+  app$set_inputs(species2 = "comrav")
+  app$set_inputs(provinces = c("CA-AB", "CA-NB", "CA-NS", "CA-BC", "CA-ON", "CA-QC", 
+      "CA-SK"))
+  app$set_inputs(provinces = c("CA-AB", "CA-NB", "CA-BC", "CA-ON", "CA-QC", "CA-SK"))
   app$expect_values()
+  app$expect_screenshot()
 })
 
 
-test_that("{shinytest2} recording: region", {
-  app <- AppDriver$new(variant = platform_variant(), name = "region", height = 789, 
-      width = 1169)
+test_that("{shinytest2} recording: diversity_byregion", {
+  app <- AppDriver$new(variant = platform_variant(), name = "diversity_byregion", 
+      height = 743, width = 788)
+  app$set_inputs(navbar = "by Province")
   app$set_inputs(navbar = "Diversity")
   app$set_inputs(radio = "region_sub")
-  app$expect_screenshot()
-})
-
-
-test_that("{shinytest2} recording: whtspa", {
-  app <- AppDriver$new(variant = platform_variant(), name = "whtspa", height = 744, 
-      width = 1169)
-  app$set_inputs(navbar = "by Province")
-  app$set_inputs(species2 = "whtspa")
-  app$set_inputs(provinces = c("CA-ON", "CA-QC", "CA-AB", "CA-BC", "CA-MB", "CA-NB", 
-      "CA-NS"))
-  app$set_inputs(provinces = c("CA-ON", "CA-QC", "CA-AB", "CA-BC", "CA-NB", "CA-NS"))
-  app$expect_screenshot()
-})
-
-
-test_that("{shinytest2} recording: region-lala", {
-  app <- AppDriver$new(variant = platform_variant(), name = "region-lala", height = 789, 
-      width = 1169)
-  app$set_inputs(navbar = "by Province")
-  app$set_inputs(radio = "region_sub")
-  app$expect_screenshot()
   app$expect_values()
+  app$expect_screenshot()
+})
+
+
+test_that("{shinytest2} recording: norfli_dataexploration", {
+  app <- AppDriver$new(variant = platform_variant(), name = "norfli_dataexploration", 
+      height = 788, width = 788)
+  app$set_inputs(species = "norfli")
+  app$set_inputs(daterange = c("2020-11-28", "2021-04-29"))
+  app$set_inputs(daterange = c("2020-11-28", "2021-02-16"))
+  app$expect_values()
+  app$expect_screenshot()
 })
